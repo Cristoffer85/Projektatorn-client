e89e1e2ed9e591da65b3acf78715509cb408e19b
Cristoffer <cristoffer_ostberg85@hotmail.com>
2025-06-28 15:10:32 +0200

    Margins to edges added in navbar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8081' // Backend URL
+    apiUrl: 'https://projektatorn-server.onrender.com'
+            //'http://localhost:8081' // Backend URL
 };
