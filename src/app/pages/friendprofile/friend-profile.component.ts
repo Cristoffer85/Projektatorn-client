e159cbd5e2dd80f66804da18e030e39df4d670af
Cristoffer <cristoffer_ostberg85@hotmail.com>
2025-07-01 00:57:22 +0200

    Remove friend extra prompt added

--- src/app/pages/friendprofile/friend-profile.component.ts
+++ src/app/pages/friendprofile/friend-profile.component.ts
@@ -11,4 +11,19 @@ export class FriendProfileComponent {
   @Input() friend: any;
   @Output() close = new EventEmitter<void>();
   @Output() remove = new EventEmitter<any>();
+
+  showConfirm = false;
+
+  confirmRemove() {
+    this.showConfirm = true;
+  }
+
+  cancelRemove() {
+    this.showConfirm = false;
+  }
+
+  doRemove() {
+    this.remove.emit(this.friend);
+    this.showConfirm = false;
+  }
 }
