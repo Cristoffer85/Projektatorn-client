7163fc0f6d8cb5525ab6a87378eddbe3dbf806c2
Cristoffer <cristoffer_ostberg85@hotmail.com>
2025-07-01 00:49:06 +0200

    remove friend button added to friend-profile

--- src/app/pages/friendprofile/friend-profile.component.ts
+++ src/app/pages/friendprofile/friend-profile.component.ts
@@ -10,4 +10,5 @@ import { CommonModule } from '@angular/common';
 export class FriendProfileComponent {
   @Input() friend: any;
   @Output() close = new EventEmitter<void>();
+  @Output() remove = new EventEmitter<any>();
 }
